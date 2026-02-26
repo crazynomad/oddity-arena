a1aab9cc982017593e26c6cab1bc304d79d10f46
Burn <burn.wang@gmail.com>
2026-02-26 08:24:25 +0800

    chore: gitignore .vercel build output + add gallery page + edge runtime fix

--- web/.vercel/output/static/_worker.js/__next-on-pages-dist__/manifest/__RSC_SERVER_MANIFEST.js
+++
@@ -1,5 +0,0 @@
-var _={},t=(e,n,o)=>(_.__RSC_SERVER_MANIFEST=e.__RSC_SERVER_MANIFEST=`{
-  "node": {},
-  "edge": {},
-  "encryptionKey": "zMAMtr/ENLpwj1drC1bThbda9zj4W2JSULjFox/qimk="
-}`,_);export{t as __getNamedExports};
